55738e95546d2c91a69b80979d6b06f3a0615f73
thejunghare <prasadjunghare2@gmail.com>
2024-02-02 15:08:05 +0530

    added default to users role_id column

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('role_id');
+            $table->unsignedBigInteger('role_id')->default(3);
             $table->string('name');
             $table->string('email')->unique();
             $table->string('phone')->unique();
