590d50b9fb656fdb4f15564e3a597a198b591fbe
thejunghare <prasadjunghare2@gmail.com>
2024-02-05 13:16:57 +0530

    - add member model - remove auditors and accountants model - add accountant model - add table to see members - add members livewire

--- app/Livewire/Members/ManageSocietiesMembersIndex.php
+++ app/Livewire/Members/ManageSocietiesMembersIndex.php
@@ -2,12 +2,19 @@
 
 namespace App\Livewire\Members;
 
+use App\Models\Member;
 use Livewire\Component;
 
 class ManageSocietiesMembersIndex extends Component
 {
     public function render()
     {
-        return view('livewire.members.manage-societies-members-index');
+        return view('livewire.members.manage-societies-members-index', [
+            'members' => Member::latest()->paginate(5),
+        ])
+            ->title('Manage societies members - Society')
+            ->with([
+                'button' => 'Create new user'
+            ]);
     }
 }
