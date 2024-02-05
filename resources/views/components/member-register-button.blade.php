841b02062f36280abaf7241c82b53dbb09e0e74b
thejunghare <prasadjunghare2@gmail.com>
2024-02-05 16:35:05 +0530

    made component for user dashboard tick and modal toggle button

---
+++ resources/views/components/member-register-button.blade.php
@@ -0,0 +1,23 @@
+<div id="alert-additional-content-4"
+            class="p-4 mb-4 text-yellow-800 border border-yellow-300 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300 dark:border-yellow-800"
+            role="alert">
+            <div class="flex items-center">
+                <svg class="flex-shrink-0 w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor" viewBox="0 0 20 20">
+                    <path
+                        d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+                </svg>
+                <span class="sr-only">Info</span>
+                <h3 class="text-lg font-medium">You are not a member, please register.</h3>
+            </div>
+            <div class="mt-2 mb-4 text-sm">
+                You need to register as a member of a society to pay bills and use other features.
+            </div>
+            <div class="flex">
+                <button data-modal-target="crud-modal" data-modal-toggle="crud-modal"
+                    class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    type="button">
+                    Register
+                </button>
+            </div>
+        </div>
