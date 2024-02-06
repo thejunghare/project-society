3e749fda0fe5a9ee243f48999b4332b06e868329
thejunghare <prasadjunghare2@gmail.com>
2024-02-06 21:38:57 +0530

    fix margin issues on user dashboard

--- resources/views/components/member-register-button.blade.php
+++ resources/views/components/member-register-button.blade.php
@@ -1,4 +1,5 @@
-<div class="mt-12 w-full  p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+<div
+    class="mt-14 md:mt-12 lg:mt-12 w-full  p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
     <a href="#">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             You are not a member, please register.</h5>
