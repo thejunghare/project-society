1c4ab778005e0e193b15cc1606ab50b732bc347e
thejunghare <prasadjunghare2@gmail.com>
2024-02-07 16:34:32 +0530

    gave basic design to check-permission

--- resources/views/errors/check-permission.blade.php
+++ resources/views/errors/check-permission.blade.php
@@ -1,1 +1,29 @@
-<p>What's up buddy</p>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Access denied!</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <div class="flex justify-center items-center h-screen">
+        <div
+            class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Excuse Me, Brother...
+                    Brother Edhar!
+                </h5>
+            </a>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">We've detected an intruder! Just kidding,
+                you've
+                just wandered into a restricted area. Best to turn back before we release the virtual guard dogs. Woof!
+            </p>
+        </div>
+    </div>
+</body>
+
+</html>
