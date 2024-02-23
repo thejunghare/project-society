6e0fdf29c667b0d7e26ec7967d6c2ea84a0408e0
thejunghare <prasadjunghare2@gmail.com>
2024-02-23 18:44:06 +0530

    - add header and footer to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -838,12 +838,27 @@
     </style>
 </head>
 
-<body class="">
-    <section class="bg-white dark:bg-gray-900 flex items-center justify-center h-screen">
+<body class="h-screen flex flex-col">
+    {{-- header --}}
+    <nav class="h-16 flex justify-center items-center">
+        <div class="w-1/2 ">
+            <p class="px-6 text-2xl italic tracking-wide subpixel-antialiased font-extrabold">
+                <a href="#" class="cursor-not-allowed text-teal-500">mySocietyERP</a>
+            </p>
+        </div>
+        <div class="w-1/2 text-right">
+            <p class="px-6 text-lg tracking-wide subpixel-antialiased font-bold">
+                <a href="#" class="underline cursor-not-allowed text-dark-500">Home</a>
+            </p>
+        </div>
+    </nav>
+
+    {{-- main section --}}
+    <section class="bg-white dark:bg-gray-900 flex items-center justify-center flex-1">
         <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
             <h1
                 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-                We invest in the world’s potential</h1>
+                We invest in the <span class="text-teal-500"> world’s potential</span></h1>
             <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Here at
                 Project Society we focus on markets where technology, innovation, and capital can unlock long-term value
                 and
@@ -880,8 +895,14 @@
         </div>
     </section>
 
-
-
+    {{-- footer --}}
+    <footer class="h-12 flex justify-center items-center">
+        <p class="font-medium">Designed, Developed by
+            <span class="underline text-teal-500">
+                <a class="cursor-not-allowed" href="#">thejunghare</a>
+            </span>
+        </p>
+    </footer>
 
     <script src="{{ asset('../node_modules/flowbite/dist/flowbite.js') }}"></script>
     {{-- <script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script> --}}
