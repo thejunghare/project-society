388274bbb3a35fe491dfd3d180ca0e2324baed32
thejunghare <prasadjunghare2@gmail.com>
2024-03-07 20:49:07 +0530

    - add start generating pdf for maintenance bills

--- app/Livewire/Members/ManageSocietiesMembersIndex.php
+++ app/Livewire/Members/ManageSocietiesMembersIndex.php
@@ -6,8 +6,12 @@ use App\Models\Member;
 use Livewire\Component;
 use App\Models\Societies;
 use Livewire\WithPagination;
+use LaravelDaily\Invoices\Invoice;
+use Spatie\LaravelPdf\Facades\Pdf;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Auth;
+use LaravelDaily\Invoices\Classes\Buyer;
+use LaravelDaily\Invoices\Classes\InvoiceItem;
 
 class ManageSocietiesMembersIndex extends Component
 {
@@ -17,7 +21,7 @@ class ManageSocietiesMembersIndex extends Component
     public $selectedSociety;
     public $search;
     public $months;
-    public $currentYear;
+    public $currentYear, $customer, $invoice, $item;
 
     public function mount()
     {
@@ -38,7 +42,27 @@ class ManageSocietiesMembersIndex extends Component
         ];
         $this->currentYear = date('Y');
     }
-    
+
+    public function generatePdf(Invoice $invoice)
+    {
+        $members = $this->selectedSociety
+            ? Member::where('society_id', $this->selectedSociety)
+                ->join('bills', 'members.id', '=', 'bills.member_id')
+                ->join('users', 'members.user_id', '=', 'users.id')
+                ->where(function ($query) {
+                    $query->where('users.name', 'like', "%{$this->search}%")
+                        ->orWhere('users.phone', 'like', "%{$this->search}%")
+                        ->orWhere('users.email', 'like', "%{$this->search}%");
+                })
+                ->select('users.name', 'users.phone', 'users.email', 'bills.id', 'bills.billing_month', 'bills.amount', 'bills.status')
+                ->latest('members.created_at')
+                ->paginate(5)
+            : collect();
+
+        return Pdf::view('pdfs.invoice', ['members' => $members])
+            ->save(storage_path('app/files/invoice.pdf'));
+    }
+
     public function render()
     {
         $members = $this->selectedSociety
---
+++ resources/views/pdfs/invoice.blade.php
@@ -0,0 +1,5 @@
+<link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
+
+
+<h1 class="text-5xl font-extrabold dark:text-white">{{$members->name}}<small
+        class="ms-2 font-semibold text-gray-500 dark:text-gray-400">This is secondary text</small></h1>
