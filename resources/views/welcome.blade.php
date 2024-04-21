1cf626b6608c9a318e0573f5a8900cd2d6ddb389
pharande <pharandepratiksha9@gmail.com>
2024-04-21 21:10:59 +0530

    add new title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Welcome form pratiksha</title>
+    <title>Project Society</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
