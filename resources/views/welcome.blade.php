757790b3ee99b277d44614c4a4d656620ae606bb
Aaditya Pravin Kolhapure <122794685+aadityakolhapure@users.noreply.github.com>
2024-04-25 18:38:07 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Project Society</title>
+    <title>Aaditya Kolhapure</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
