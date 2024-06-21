ef1c841a729ef14a5bdac67ca6da16987964fe35
thejunghare <prasadjunghare2@gmail.com>
2024-06-21 22:40:44 +0530

    fetched members from society

--- app/Livewire/Members/ManageSocietiesMembersIndex.php
+++ app/Livewire/Members/ManageSocietiesMembersIndex.php
@@ -1,36 +1,77 @@
 <?php
 
+
 namespace App\Livewire\Members;
 
 use Livewire\Component;
 use App\Models\Societies;
+use App\Models\Member;
+use App\Models\User;
 use Livewire\Attributes\Title;
 
 class ManageSocietiesMembersIndex extends Component
 {
-    #[Title('Manage members - mySocietyERP')]
-    public $societyId;
-    public $society;
-    // public $members;
+  #[Title('Manage members - mySocietyERP')]
+  public $societyId;
+  public $society;
 
-    public function mount($society)
-    {
-        $this->societyId = $society;
-        $this->loadSocietyMembers($this->societyId);
-    }
+  public $editingMember = null;
+  public $name, $phone, $room_number, $is_rented;
+/*   public $checkedMember = []; */
 
-    public function loadSocietyMembers($societyId)
-    {
-        $this->society = Societies::with('members')->findOrFail($societyId);
-    }
+  public function mount($society)
+  {
+    $this->societyId = $society;
+    $this->loadSocietyMembers($this->societyId);
+  }
 
-    public function render()
-    {
-        $this->members = $this->society->members()->paginate(5);
-
-        return view('livewire.members.manage-societies-members-index', [
-            'society' => $this->society,
-            'members' => $this->members,
-        ]);
-    }
+ /*  public function isChecked($memberId){
+    return in_array($memberId, $this->checkedMember) ? 'bg-info text-white' : '';
 }
+ */
+  public function loadSocietyMembers($societyId)
+  {
+    $this->society = Societies::with('members')->findOrFail($societyId);
+  }
+
+  public function startEdit($memberId)
+  {
+    $member = Member::with('user')->findorFail($memberId);
+    $this->editingMember = $member->id;
+    $this->name = $member->user->name;
+    $this->phone = $member->user->phone;
+    $this->room_number = $member->room_number;
+    $this->is_rented = $member->is_rented;
+  }
+
+  public function cancelEdit()
+  {
+    $this->editingMember = null;
+  }
+
+  public function save($id)
+  {
+    $member = Member::findOrFail($this->editingMember);
+    $member->room_number = $this->room_number;
+    $member->is_rented = $this->is_rented;
+    $member->save();
+
+    $user = User::findOrFail($member->user_id);
+    $user->name = $this->name;
+    $user->phone = $this->phone;
+    $user->save();
+
+    $this->editingMember = null;
+  }
+
+  public function render()
+  {
+    $this->members = $this->society->members()->paginate(5);
+
+    return view('livewire.members.manage-societies-members-index', [
+      'society' => $this->society,
+      'members' => $this->members,
+    ]);
+  }
+}
+
--- resources/views/livewire/members/manage-societies-members-index.blade.php
+++ resources/views/livewire/members/manage-societies-members-index.blade.php
@@ -1,5 +1,8 @@
 <div class="mt-12">
     {{-- The Master doesn't talk, he acts. --}}
+
+
+
     <div class="relative overflow-x-auto">
         <div
             class="flex items-center justify-between flex-column md:flex-row flex-wrap space-y-4 md:space-y-0 py-4 bg-white dark:bg-gray-900">
@@ -22,7 +25,8 @@
 
                         <li>
                             <a href="#"
-                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Add Member</a>
+                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Add
+                                Member</a>
                         </li>
                     </ul>
                     <div class="py-1">
@@ -46,16 +50,13 @@
                     placeholder="Search for members">
             </div>
         </div>
+
+
+
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
-                    <th scope="col" class="p-4">
-                        <div class="flex items-center">
-                            <input id="checkbox-all-search" type="checkbox"
-                                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                            <label for="checkbox-all-search" class="sr-only">checkbox</label>
-                        </div>
-                    </th>
+
                     <th scope="col" class="px-6 py-3">
                         Name
                     </th>
@@ -75,13 +76,7 @@
                 @foreach ($members as $member)
                     <tr
                         class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                        <td class="w-4 p-4">
-                            <div class="flex items-center">
-                                <input id="checkbox-table-search-1" type="checkbox"
-                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
-                            </div>
-                        </td>
+
                         <th scope="row"
                             class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
 
@@ -102,9 +97,17 @@
                         </td>
                         <td class="px-6 py-4">
                             <!-- Modal toggle -->
-                            <a href="#" type="button" data-modal-target="editUserModal"
+                            {{--   <a href="#" type="button" data-modal-target="editUserModal"
                                 data-modal-show="editUserModal"
-                                class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a> --}}
+                            <button wire:click="startEdit({{ $member->id }})"
+                                class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                                Edit
+                            </button>
+                            <button wire:click="startEdit({{ $member->id }})"
+                                class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                                Delete
+                            </button>
                         </td>
                     </tr>
                 @endforeach
@@ -116,102 +119,76 @@
         </div>
 
 
-        <div id="editUserModal" tabindex="-1" aria-hidden="true"
-            class="fixed top-0 left-0 right-0 z-50 items-center justify-center hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-            <div class="relative w-full max-w-2xl max-h-full">
-                <!-- Modal content -->
-                <form class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                    <!-- Modal header -->
-                    <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
-                        <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                            Edit user
-                        </h3>
-                        <button type="button"
-                            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                            data-modal-hide="editUserModal">
-                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                viewBox="0 0 14 14">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                    stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                            </svg>
-                            <span class="sr-only">Close modal</span>
-                        </button>
-                    </div>
-                    <!-- Modal body -->
-                    <div class="p-6 space-y-6">
-                        <div class="grid grid-cols-6 gap-6">
-                            <div class="col-span-6 sm:col-span-3">
-                                <label for="first-name"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First
-                                    Name</label>
-                                <input type="text" name="first-name" id="first-name"
-                                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    placeholder="Bonnie" required="">
-                            </div>
-                            <div class="col-span-6 sm:col-span-3">
-                                <label for="last-name"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last
-                                    Name</label>
-                                <input type="text" name="last-name" id="last-name"
-                                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    placeholder="Green" required="">
-                            </div>
-                            <div class="col-span-6 sm:col-span-3">
-                                <label for="email"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                                <input type="email" name="email" id="email"
-                                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    placeholder="example@company.com" required="">
-                            </div>
-                            <div class="col-span-6 sm:col-span-3">
-                                <label for="phone-number"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone
-                                    Number</label>
-                                <input type="number" name="phone-number" id="phone-number"
-                                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    placeholder="e.g. +(12)3456 789" required="">
-                            </div>
-                            <div class="col-span-6 sm:col-span-3">
-                                <label for="department"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Department</label>
-                                <input type="text" name="department" id="department"
-                                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    placeholder="Development" required="">
-                            </div>
-                            <div class="col-span-6 sm:col-span-3">
-                                <label for="company"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Company</label>
-                                <input type="number" name="company" id="company"
-                                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    placeholder="123456" required="">
-                            </div>
-                            <div class="col-span-6 sm:col-span-3">
-                                <label for="current-password"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Current
-                                    Password</label>
-                                <input type="password" name="current-password" id="current-password"
-                                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    placeholder="••••••••" required="">
-                            </div>
-                            <div class="col-span-6 sm:col-span-3">
-                                <label for="new-password"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New
-                                    Password</label>
-                                <input type="password" name="new-password" id="new-password"
-                                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    placeholder="••••••••" required="">
+        @if ($editingMember)
+            <div id="editUserModal" tabindex="-1" aria-hidden="true"
+                class="fixed top-0 left-0 right-0 z-50 items-center justify-center hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                <div class="relative w-full max-w-2xl max-h-full">
+                    <!-- Modal content -->
+                    <form class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                        <!-- Modal header -->
+                        <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                            <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                                Edit member
+                            </h3>
+                            <button type="button"
+                                class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                data-modal-hide="editUserModal">
+                                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                    fill="none" viewBox="0 0 14 14">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                </svg>
+                                <span class="sr-only">Close modal</span>
+                            </button>
+                        </div>
+                        <!-- Modal body -->
+                        <div class="p-6 space-y-6">
+                            <div class="grid grid-cols-6 gap-6">
+                                <div class="col-span-6 sm:col-span-3">
+                                    <label for="first-name"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Full
+                                        Name</label>
+                                    <input type="text" name="first-name" id="first-name"
+                                        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        wire:modal='name' placeholder="Prasad Shridhar Junghare" required="">
+                                </div>
+                                <div class="col-span-6 sm:col-span-3">
+                                    <label for="last-name"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Room
+                                        Number</label>
+                                    <input type="text" name="last-name" id="last-name"
+                                        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        placeholder="1132" required="">
+                                </div>
+                                <div class="col-span-6 sm:col-span-3">
+                                    <label for="email"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                                    <input type="email" name="email" id="email"
+                                        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        placeholder="example@company.com" required="">
+                                </div>
+                                <div class="col-span-6 sm:col-span-3">
+                                    <label for="phone-number"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone
+                                        Number</label>
+                                    <input type="number" name="phone-number" id="phone-number"
+                                        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        placeholder="e.g. +(12)3456 789" required="">
+                                </div>
+
                             </div>
                         </div>
-                    </div>
-                    <!-- Modal footer -->
-                    <div
-                        class="flex items-center p-6 space-x-3 rtl:space-x-reverse border-t border-gray-200 rounded-b dark:border-gray-600">
-                        <button type="submit"
-                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Save
-                            all</button>
-                    </div>
-                </form>
+                        <!-- Modal footer -->
+                        <div
+                            class="flex items-center p-6 space-x-3 rtl:space-x-reverse border-t border-gray-200 rounded-b dark:border-gray-600">
+                            <button type="submit"
+                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Save
+                                all</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
+        @endif
+
     </div>
 </div>
